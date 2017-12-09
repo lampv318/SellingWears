f31f662fe9575a6c20efdc72276cdd4c5587c4dc
lampv <lampvhy@gmail.com>
2017-12-09 12:14:18 +0700

    admin_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper 
+
+  private
+  
+  def check_login
+    unless logged_in?
+      flash[:danger] = t "login_first"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    unless current_user.is_admin?
+      flash[:danger] = t "not_admin"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
+    unless @product
+      flash[:danger] = "not_found"
+      redirect_to root_path
+    end
   end
 
   def index
