c7a7d94489c3ccd4a36eb020327b01060ddeb1d8
lampv <lampvhy@gmail.com>
2017-12-15 09:58:35 +0700

    fix add number to cart

--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -4,6 +4,12 @@ class OrderLinesController < ApplicationController
   def create
     @order = current_order
     @order_line = @order.order_lines.new order_line_params
+    @order_lines = @order.order_lines
+    current_line = @order.order_lines.find_by(product_id: @order_line.product_id)
+    if current_line
+      @order_line.number +=  current_line.number
+      @order_lines.find_by(product_id: @order_line.product_id).destroy
+    end
     @order.save
     flash[:success] = "Order save"  
     session[:order_id] = @order.id
