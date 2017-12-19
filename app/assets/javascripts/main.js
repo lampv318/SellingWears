8f1a8e7d8b7f2f86bcf84ae706669569c83166f9
hungdomanh <hung150896@gmail.com>
2017-12-19 22:36:26 +0700

    product detail, no photo

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,6 +4,33 @@ $(function(){
 		console.log("obj");
 	})
 
+	$('.action.add-to-cart').click(function() {
+		var productId = $(this).data('product-id');
+		var quantity = $('#AddToCartForm .quantity .qty').val();
+		
+		$.ajax({
+			type: "POST",
+			url: "/order_line",
+			data: 'number=' + quantity + "&product_id=" + productId,
+			dataType: 'json',
+			beforeSend: function() {
+				// ShowLoading();
+			},
+			success: function(msg) {
+				HideLoading();
+				$('.canvas-menu-toggle.sn-back-to-top i').animate({fontSize: '160%'},400).animate({fontSize: '100%'},500);
+				resetCart(showCart);
+			},
+			error: function(xhr, text) {
+				if(text == 'error' && xhr.responseJSON.status == 422){
+					AlertError(xhr.responseJSON.description);
+				}else{
+					AlertError("");
+				}
+			}
+		});
+	})
+
 });
 
 $(function() { engoPriceSlider(); });
