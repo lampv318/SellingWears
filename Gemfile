ec4db3e8a544fae7513574ad7e26f3cd5bbbe76c
hungdomanh <hung150896@gmail.com>
2017-12-19 14:24:11 +0700

    browser_syns

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development do
   gem "listen", ">= 3.0.5", "< 3.2"
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
+  gem 'browser_sync_rails'
 end
 
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ app/assets/javascripts/ajax.js
@@ -0,0 +1,12 @@
+$(function(){
+//   $(".new_order_line").submit(function(event){
+//     event.preventDefault();
+// console.log('sdfsdf');
+//     // this debugger should be hit when you click the submit button!
+//     debugger;
+//   });
+	$('.text-ajax').click(function(){
+		console.log("obj");
+	})
+
+});
