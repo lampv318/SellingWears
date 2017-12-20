31f2ea28ee0427b962816340d5fd373d9ad8c888
lampv <lampvhy@gmail.com>
2017-12-20 21:38:52 +0700

    fix cart row 2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,11 +15,9 @@ class OrdersController < ApplicationController
 
   def update
     if current_order.update_attributes order_params
-      flash[:success] = "Buy Success"
       session.delete :order_id
       redirect_to root_path
     else
-      flash[:danger] = "Can't buy"
       redirect_to cart_path
     end
   end
