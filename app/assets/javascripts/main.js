0c5ffa7f76f7d29dcf2d8ca0454ccddebad67d44
hungdomanh <hung150896@gmail.com>
2017-12-21 13:28:07 +0700

    fix header

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,14 +1,46 @@
+// import AliExpress from ‘aliexpress’
+
 $(function(){
 
 	$('.text-ajax').click(function(){
 		console.log("obj");
 	});
+		// AliExpress.BestSelling.get().then(function(goods){
+		// 	console.log('Best Selling items:');
+
+		// 	console.log(goods);
+		// });
 
 	$('.action.add-to-cart').click(function(){
 		var productId = $(this).data('product-id');
 		var quantity  = $("#AddToCartForm .qty").val();
 
-		console.log(productId);
+		$.ajax({
+        type: "post",
+        url: "/order_lines",
+        data: 'order_line[number]=' + quantity + "&order_line[product_id]=" + productId,
+        // dataType: 'json',
+        beforeSend: function() {
+            // ShowLoading();
+            console.log("--------");
+        },
+        success: function(msg) {
+            // HideLoading();
+            // $('.canvas-menu-toggle.sn-back-to-top i').animate({fontSize: '160%'},400).animate({fontSize: '100%'},500);
+            // resetCart(showCart);
+            console.log('sdfsdfsd');
+        },
+        error: function(xhr, text) {
+            // console.log('ahii');
+            console.error(xhr);
+            console.log(text);
+            // if(text == 'error' && xhr.responseJSON.status == 422){
+                // AlertError(xhr.responseJSON.description);
+            // }else{
+                // AlertError("");
+            // }
+        }
+    });
 	});	
 
 });
