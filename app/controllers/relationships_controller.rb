87d41d1b0d5599076a110f52218c70f320377f8d
lampv <lampvhy@gmail.com>
2017-12-26 22:07:49 +0700

    hot fix wishlist controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
 
   def create
     product = Product.find params[:relationship][:product_id]
-    if current_user.products.find params[:relationship][:product_id]
+    if current_user.products.find_by id: params[:relationship][:product_id]
       flash[:danger] = "Existed !"
       redirect_to products_path
     else
